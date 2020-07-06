b756def1fb2d24476483635e55bcfd483d3c19f4
stephane71 <stephanemaire71@gmail.com>
2020-07-06 17:02:01 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,196 +1,140 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import departmentList from "../public/departments.json";
+
+const DEFAULT_DEPARTMENT = departmentList.find(({ code }) => code === "78");
+const DEFAULT_POPULATION = 20000;
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [population, setPopulation] = useState(DEFAULT_POPULATION);
+  const [cities, setCities] = useState([]);
+  const [filteredCities, setFilteredCities] = useState([]);
+  const [selectedDepartment, setSelectedDepartment] = useState(
+      DEFAULT_DEPARTMENT
+  );
+
+  useEffect(() => {
+    if (selectedDepartment && selectedDepartment.code) {
+      setLoading(true);
+      fetch(
+          `https://geo.api.gouv.fr/departements/${selectedDepartment.code}/communes`
+      )
+          .then(data => data.json())
+          .then(data => {
+            setCities(data);
+            setFilteredCities(data.filter(city => city.population >= population));
+            setTimeout(() => setLoading(false), 2000);
+          });
+    }
+  }, [selectedDepartment]);
+
+  function handleChangeDepartment(event) {
+    const dep = departmentList.find(({ code }) => code === event.target.value);
+    setSelectedDepartment(dep);
+  }
+
+  function handleChangePopulation(event) {
+    if (!event.target.value) {
+      return;
+    }
+    setPopulation(Number.parseInt(event.target.value));
+  }
+
+  function handleClickValidation() {
+    setFilteredCities(cities.filter(city => city.population >= population));
+  }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className="container">
+        <Head>
+          <title>Les communes francaises</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        <main>
+          <header>
+            <h1 className="title">Les communes françaises</h1>
+            <p className="description">
+              Rechercher la liste des communes par département selon leur
+              population
             </p>
-          </a>
-        </div>
-      </main>
+          </header>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+          <div className="search">
+            <div className="form">
+              <select
+                  className="department"
+                  onChange={handleChangeDepartment}
+                  value={selectedDepartment?.code}
+              >
+                <option value="">Choississez un departement</option>
+                {departmentList.map(dep => (
+                    <option value={dep.code} key={dep.code}>
+                      {dep.code} - {dep.nom}
+                    </option>
+                ))}
+              </select>
+              <div className="population">
+                <input
+                    type="number"
+                    placeholder="population supérieur à"
+                    value={population}
+                    onChange={handleChangePopulation}
+                    min={0}
+                />
+                <button onClick={handleClickValidation}>Valider</button>
+              </div>
+            </div>
+          </div>
+          <div className="results">
+            {loading && <div>Chargement...</div>}
+            {!loading && !filteredCities.length && <div>Pas de résultats</div>}
+            {filteredCities.map(({ nom, code, population }) => (
+                <div key={code}>{`${code} - ${nom}: ${population}`}</div>
+            ))}
+          </div>
+        </main>
 
-      <style jsx>{`
+        <style jsx>{`
         .container {
           min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          padding: 24px;
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .results {
+          flex-grow: 1;
         }
 
-        footer img {
-          margin-left: 0.5rem;
+        .form > .department,
+        .form > .population > input,
+        .form > .population > button {
+          padding: 4px;
         }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .department {
+          margin-bottom: 16px;
         }
 
-        a {
-          color: inherit;
-          text-decoration: none;
+        .population > input {
+          margin-right: 16px;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .population {
+          display: block;
+          margin-bottom: 16px;
         }
       `}</style>
 
-      <style jsx global>{`
+        <style jsx global>{`
         html,
         body {
           padding: 0;
@@ -204,6 +148,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
-  )
+      </div>
+  );
 }
