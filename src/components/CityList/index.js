a90b609a01ce62869947f53cedbba8a02fe5fb87
stephane71 <stephanemaire71@gmail.com>
2020-07-07 14:00:44 +0200

    CityList | add dense prop to List CityList | update variant of primary ListItemText

--- src/components/CityList/index.js
+++ src/components/CityList/index.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { CITY_NAME_ATTRIBUTE, CITY_POPULATION_ATTRIBUTE } from "../../enums";
+
+import { CITY_NAME_ATTRIBUTE, CITY_POPULATION_ATTRIBUTE } from "src/enums";
 
 function CityList({ cityList }) {
   if (!cityList.length) {
@@ -11,11 +12,12 @@ function CityList({ cityList }) {
   }
 
   return (
-    <List>
+    <List dense>
       {cityList.map(city => (
         <ListItem key={city.code} button>
           <ListItemText
             primary={city[CITY_NAME_ATTRIBUTE]}
+            primaryTypographyProps={{ variant: "subtitle2" }}
             secondary={new Intl.NumberFormat("fr-FR").format(city.code)}
           />
           <span>
